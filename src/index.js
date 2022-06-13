8fc98f79db4095b45ff21759c3bd8926e475f3e8
northkuria <kuri4martin@gmail.com>
2022-06-13 12:22:32 +0300

    added tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   // your code here
-});
+  function addTask(e) {
+    e.preventDefault()
+    const task = document.getElementById('new-task-description').value
+
+    const node = document.createElement('li')
+
+    const textnode = document.createTextNode(task)
+
+    node.appendChild(textnode)
+
+    document.getElementById('tasks').appendChild(node)
+  }
+
+  const form = document.getElementById('create-task-form')
+  form.addEventListener('submit', addTask)
+})
